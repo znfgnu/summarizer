5106ce6ed7d0ec3149fa84110ec683a7b55c5276
Lukasz Marcul <dev.marcul@gmail.com>
2019-05-28 23:42:43 +0200

    Setup multi-article load

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from PyTeaserPython3 import pyteaser
 
-if __name__ == "__main__":
-    print("Hello")
-    url = "https://www.the-gazette.co.uk/news/17666505.european-elections-2019-snp-landslide-as-voters-abandon-labour/"
-    result = pyteaser.SummarizeUrl(url)
+urls = []
+urls.append('http://www.espnfc.com/english-premier-league/23/blog/post/3862792/champions-league-europa-league-success-for-arsenal-chelsea-and-spurs-shows-power-of-london-effect')
+urls.append('https://rare-technologies.com/text-summarization-in-python-extractive-vs-abstractive-techniques-revisited/')
 
-for sentence in result:
-    print(sentence)
+results_pyteaser = {}
+
+
+def print_result(sentences):
+    if sentences is None:
+        return
+    for sentence in sentences:
+        print(sentence)
+
+
+def summarize_pyteaser(url):
+    return pyteaser.SummarizeUrl(url)
+
+
+if __name__ == "__main__":
+    for url in urls:
+        results_pyteaser[url] = summarize_pyteaser(url)
+
+    for res in results_pyteaser:
+            print_result(results_pyteaser[res])
+            print()
